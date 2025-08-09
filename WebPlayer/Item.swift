e191edcddae8cfdec8dbe749d26268f3e1bf6643
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-08-08 22:32:50 -0400

    class WebRefModel : NSObject

---
+++ WebPlayer/Item.swift
@@ -0,0 +1,36 @@
+//
+//  Refs.swift
+//  WebPlayer
+//
+//  Created by jht2 on 8/8/25.
+//
+
+
+let items = [
+  Item(label:"color-twinz-umd",ref:"sketches/color-twinz-umd/index.html"),
+  Item(label:"color-twinz-beta.vercel",ref:"https://color-twinz-beta.vercel.app/"),
+  Item(label:"sketches/dist", ref:"sketches/dist/index.html"),
+  Item(label:"p5videoKit", ref:"https://jht1493.net/p5videoKit/demo/"),
+  Item(label:"sketches/shapes_random_pause_v22", ref:"sketches/shapes_random_pause_v22/index.html"),
+  Item(label:"faceMesh group=s1 ", ref:"https://molab-itp.github.io/moSalon/src/faceMesh/index.html?&v=50&group=s1"),
+  Item(label:"faceMesh", ref:"https://molab-itp.github.io/moSalon/src/faceMesh/qrcode/?v=50"),
+  Item(label:"sketches/shapes_random_pause_v22", ref:"sketches/shapes_random_pause_v22/index.html"),
+  Item(label:"sketches/ims04-jht-scroll-color-v2/", ref:"sketches/ims04-jht-scroll-color-v2/index.html"),
+  Item(label:"ims04-jht scroll color rate", ref:"https://jht9629-nyu.github.io/p5mirror-jht9629-nyu/p5projects/ims04-jht%20scroll%20color%20rate-2pxhnehBV/"),
+  Item(label:"shapes random pause", ref:"https://jht9629-nyu.github.io/p5mirror-jht9629-nyu/p5projects/shapes%20random%20pause%20v22-n0LYuXRmX/?v=1"),
+  Item(label:"n0LYuXRmX", ref:"https://editor.p5js.org/jht9629-nyu/sketches/n0LYuXRmX"),
+  Item(label:"2pxhnehBV", ref:"https://editor.p5js.org/jht9629-nyu/sketches/2pxhnehBV"),
+  Item(label:"itp.nyu.edu", ref:"https://itp.nyu.edu"),
+  Item(label:"Flag_of_Jamaica", ref:"https://upload.wikimedia.org/wikipedia/commons/0/0a/Flag_of_Jamaica.svg"),
+  Item(label:"Flag_of_Guyana", ref:"https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_Guyana.svg"),
+  Item(label:"Flag_of_the_United_States", ref:"https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"),
+  Item(label:"Flag_of_%C3%85land", ref:"https://upload.wikimedia.org/wikipedia/commons/5/52/Flag_of_%C3%85land.svg"),
+  Item(label:"Flag_of_Zimbabwe", ref:"https://upload.wikimedia.org/wikipedia/commons/6/6a/Flag_of_Zimbabwe.svg"),
+  Item(label:"Flag_of_the_Taliban", ref:"https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"),
+  Item(label:"www.apple.com", ref:"https://www.apple.com"),
+]
+
+struct Item {
+  let label: String
+  let ref: String
+}
---
+++ WebPlayer/WebRefModel.swift
@@ -0,0 +1,117 @@
+class WebRefModel : NSObject, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler  {
+  var webView: WKWebView?
+  
+  func makeWebView() -> WKWebView  {
+    if let webView  {
+      return webView
+    }
+    let webConfiguration = WKWebViewConfiguration()
+    webConfiguration.allowsInlineMediaPlayback = true;
+    // webConfiguration.allowsAirPlayForMediaPlayback = true;
+    // webConfiguration.allowsPictureInPictureMediaPlayback = true;
+    webConfiguration.mediaTypesRequiringUserActionForPlayback = [];
+    
+    let webController = WKUserContentController()
+    webController.add(self, name: "dice")
+    webConfiguration.userContentController = webController;
+    
+    let wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+    
+    wkWebView.uiDelegate = self
+    wkWebView.navigationDelegate = self
+    
+    print("makeUIView wkWebView", wkWebView as Any)
+    webView = wkWebView;
+
+    return wkWebView
+  }
+
+  func userContentController(
+    _ userContentController: WKUserContentController,
+    didReceive message: WKScriptMessage
+  ) {
+    xprint("userContentController didReceive message", message)
+    xprint("userContentController didReceive message.name", message.name)
+    xprint("userContentController didReceive message.body", message.body)
+    guard message.name == "dice" else {
+      return
+    }
+    guard let body = message.body as? NSDictionary else {
+      return
+    }
+    xprint("userContentController body", body)
+    guard let stats = body["stats"] as? NSDictionary else {
+      return
+    }
+    xprint("userContentController stats", stats)
+    if let init_lapse = stats["init_lapse"] as? Double {
+      xprint("userContentController init_lapse", init_lapse)
+//      parent.update( key: "init_lapse", value: init_lapse )
+    }
+    if let load_lapse = stats["load_lapse"] as? Double {
+      xprint("userContentController load_lapse", load_lapse)
+//      parent.update( key: "load_lapse", value: load_lapse )
+    }
+    // load_lapse
+  }
+  
+  func webView(
+    _ webView: WKWebView,
+    requestDeviceOrientationAndMotionPermissionFor origin: WKSecurityOrigin,
+    initiatedByFrame frame: WKFrameInfo,
+    decisionHandler: @escaping (WKPermissionDecision) -> Void
+  ) {
+    xprint("requestDeviceOrientationAndMotionPermissionFor origin", origin)
+    decisionHandler(.grant);
+  }
+  
+  func webView(
+    _ webView: WKWebView,
+    requestMediaCapturePermissionFor origin: WKSecurityOrigin,
+    initiatedByFrame frame: WKFrameInfo,
+    type: WKMediaCaptureType,
+    decisionHandler: @escaping (WKPermissionDecision) -> Void
+  ) {
+    xprint("requestMediaCapturePermissionFor origin type", type.rawValue)
+    // xprint("requestMediaCapturePermissionFor origin", origin, "type", type.rawValue)
+    decisionHandler(.grant);
+  }
+  
+  func webView(
+    _ webView: WKWebView,
+    decidePolicyFor navigationAction: WKNavigationAction,
+    preferences: WKWebpagePreferences,
+    decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void
+  ) {
+    xprint("decidePolicyFor preferences navigationAction")
+    // xprint("decidePolicyFor preferences navigationAction", navigationAction, "preferences", preferences)
+    decisionHandler(.allow, preferences)
+    
+    // !!@ Links failing to load - force it here.
+    //
+    if (navigationAction.navigationType != .other) {
+      webView.load(navigationAction.request);
+    }
+    
+  }
+  
+  func webView(
+    _ webView: WKWebView,
+    decidePolicyFor navigationAction: WKNavigationAction,
+    decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+  ) {
+    xprint("decidePolicyFor navigationAction")
+    // xprint("decidePolicyFor navigationAction", navigationAction)
+    decisionHandler(.allow)
+  }
+  
+  func webView(
+    _ webView: WKWebView,
+    decidePolicyFor navigationResponse: WKNavigationResponse,
+    decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void
+  ) {
+    xprint("decidePolicyFor navigationResponse")
+    // xprint("decidePolicyFor navigationResponse", navigationResponse)
+    decisionHandler(.allow)
+  }
+}
